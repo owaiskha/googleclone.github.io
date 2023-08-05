aa60b2ccea8d4924f7b8b930b99ce81564bda9e1
Owais Khan <ak2644382@gmail.com>
2023-08-06 00:26:05 +0500

    search page is added

---
+++ index.js
@@ -0,0 +1,6 @@
+const searchForm = document.querySelector("search-form");
+const searchFormInput = document.querySelector("input");
+const micBtn = document.querySelector("button");
+const img = document.querySelector(".img");
+
+
